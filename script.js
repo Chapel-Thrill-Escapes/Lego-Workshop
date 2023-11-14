a685ce2c54db1148b75a874a8def30df0ef977a3
Riley Harper <riley.harper@unc.edu>
2023-11-14 17:16:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('spinButton').addEventListener('click', function() {
-    var result = Math.floor(Math.random() * 3) + 1;
+    var result = getWeightedResult();
     var displayText = '';
 
     switch(result) {
@@ -16,3 +16,22 @@ document.getElementById('spinButton').addEventListener('click', function() {
 
     document.getElementById('result').innerText = displayText;
 });
+
+function getWeightedResult() {
+    var weights = {
+        1: 45, // Builder
+        2: 35, // Representative
+        3: 20  // Distorter
+    };
+
+    var totalWeight = 100; // Sum of the weights (45 + 35 + 20)
+    var random = Math.random() * totalWeight;
+    var weightSum = 0;
+
+    for (var i in weights) {
+        weightSum += weights[i];
+        if (random <= weightSum) {
+            return parseInt(i);
+        }
+    }
+}
